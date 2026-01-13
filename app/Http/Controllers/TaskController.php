d71ea083f2d958ad1ff722b12c1840844d0b4c0f
Carll001 <carljustin.carreon@analytica.ph>
2026-01-13 17:01:08 +0800

    feat: add Department and Task management functionality with requests and migrations

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreTaskRequest;
+use App\Http\Requests\UpdateTaskRequest;
+use App\Http\Services\TaskService;
+use App\Models\Task;
+
+class TaskController extends Controller
+{
+    protected TaskService $taskService;
+
+    public function __construct(TaskService $taskService)
+    {
+        $this->taskService = $taskService;
+    }
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTaskRequest $request)
+    {
+        $this->taskService->store();
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Task $task)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Task $task)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTaskRequest $request, Task $task)
+    {
+        $this->taskService->update();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Task $task)
+    {
+        //
+    }
+}
---
+++ app/Http/Services/TaskService.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Services;
+
+class TaskService
+{
+
+    public function store()
+    {
+        dd('store');
+    }
+
+
+    public function update()
+    {
+        dd('update');
+    }
+
+}
+?>
