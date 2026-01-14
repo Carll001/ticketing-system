d952dddebce62bcf4c49bdc6b7957ead4f548cf2
Carll001 <carljustin.carreon@analytica.ph>
2026-01-14 10:14:27 +0800

    commit

--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,6 @@ Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+// Route::middleware
+
 require __DIR__.'/settings.php';
